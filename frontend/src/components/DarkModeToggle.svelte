a6629e7835716e9b3798fcec561930489b96cac2
Isaac Harris-Holt <isaac@harris-holt.com>
2022-08-14 16:49:13 +0100

    Add drop shadow to light mode toggle

--- frontend/src/components/DarkModeToggle.svelte
+++ frontend/src/components/DarkModeToggle.svelte
@@ -5,6 +5,6 @@
 
 <div class="fixed top-8 right-8 z-10">
     <button on:click={() => dark = !dark}>
-        <i class="{icon} fa-solid text-3xl text-black dark:text-white"></i>
+        <i class="{icon} fa-solid md:text-3xl text-4xl text-black dark:text-white drop-shadow-[0_3px_4px_rgba(0,0,0,0.45)]"></i>
     </button>
 </div>
