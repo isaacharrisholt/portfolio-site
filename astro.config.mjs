484c30d6ccf440ef2c0d8a6fb3339af1c6269473
Isaac Harris-Holt <isaac@harris-holt.com>
2023-09-14 23:42:24 +0200

    v2: set up extensions and vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,13 @@
 import { defineConfig } from 'astro/config';
+import vercel from "@astrojs/vercel/static";
+import tailwind from "@astrojs/tailwind";
+import mdx from "@astrojs/mdx";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: "static",
+  adapter: vercel(),
+  integrations: [tailwind(), mdx(), sitemap()]
+});
