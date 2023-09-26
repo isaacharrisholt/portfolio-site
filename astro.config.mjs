47c0ff8a3c8fb5a863abd22e5eecc854092cc0b0
Isaac Harris-Holt <isaac@harris-holt.com>
2023-09-26 21:31:55 +0100

    jiggle jiggle

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
-import { defineConfig } from 'astro/config'
-import vercel from '@astrojs/vercel/static'
-import tailwind from '@astrojs/tailwind'
-import mdx from '@astrojs/mdx'
+import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel/static';
+import tailwind from '@astrojs/tailwind';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
 
-import sitemap from '@astrojs/sitemap'
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
   adapter: vercel(),
-  integrations: [tailwind(), mdx(), sitemap()]
-})
+  integrations: [tailwind(), mdx(), sitemap(), svelte()]
+});
