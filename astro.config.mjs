d4028611a6c8ae153ecda60966672226929f6c2c
Isaac Harris-Holt <isaac@harris-holt.com>
2024-07-23 20:21:04 +0100

    updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import vercel from '@astrojs/vercel/serverless'
 import tailwind from '@astrojs/tailwind'
 import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
-
 import svelte from '@astrojs/svelte'
 
 // https://astro.build/config
--- src/pages/codecrafters.ts
+++ src/pages/codecrafters.ts
@@ -1,16 +1,13 @@
-import type { APIRoute } from "astro";
+import type { APIRoute } from 'astro'
 
 export const prerender = false
 
 export const GET: APIRoute = async () => {
   // Redirect to codecrafters.io
-  return new Response(
-    null,
-    {
-      status: 301,
-      headers: {
-        Location: "https://app.codecrafters.io/join?via=isaacharrisholt",
-      },
+  return new Response(null, {
+    status: 301,
+    headers: {
+      Location: 'https://app.codecrafters.io/join?via=isaacharrisholt',
     },
-  );
+  })
 }
